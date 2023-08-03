8f7ad51038e8d2b8232715edc1e847800cf6cc6e
Andrej <krucfalushij@email.cz>
2023-08-03 17:02:19 +0200

    finaly2

--- src/Components/Timer/TimWedding.jsx
+++ src/Components/Timer/TimWedding.jsx
@@ -58,25 +58,33 @@ export const TimWedding = forwardRef(({ props }, ref) => {
         }}
       >
         <div className="counter">{date.day}</div>
-        <Typography sx={{ fontSize: "8px", textAlign: "center" }}>
+        <Typography
+          sx={{ fontSize: "8px", textAlign: "center", padding: "0px 5px" }}
+        >
           {date.day <= 4 ? "ДНЯ" : "ДНІВ"}
         </Typography>
       </Box>
       <Box>
         <div className="counter">{date.hours}</div>
-        <Typography sx={{ fontSize: "8px", textAlign: "center" }}>
+        <Typography
+          sx={{ fontSize: "8px", textAlign: "center", padding: "0px 5px" }}
+        >
           {date.hours <= 4 && date.hours !== 0 ? "ГОДИНИ" : "ГОДИН"}
         </Typography>{" "}
       </Box>
       <Box>
         <div className="counter">{date.minutes}</div>
-        <Typography sx={{ fontSize: "8px", textAlign: "center" }}>
+        <Typography
+          sx={{ fontSize: "8px", textAlign: "center", padding: "0px 5px" }}
+        >
           {date.minutes <= 4 ? "МИНУТИ" : "МИНУТ"}
         </Typography>{" "}
       </Box>
       <Box>
         <div className="counter">{date.seconds}</div>
-        <Typography sx={{ fontSize: "8px", textAlign: "center" }}>
+        <Typography
+          sx={{ fontSize: "8px", textAlign: "center", padding: "0px 5px" }}
+        >
           {date.seconds <= 4 ? "СЕКУНДИ" : "СЕКУНД"}
         </Typography>{" "}
       </Box>
